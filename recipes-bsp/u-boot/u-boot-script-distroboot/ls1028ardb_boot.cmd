428cdcc70bf45879db0d514ddcbb3841b4cb3f4f
Eric <eric.guo@nxp.com>
2022-06-29 18:21:27 +0800

    Increase CMA malloc memory size to 640M, otherwise display cannot work
    Signed-off-by: Eric <eric.guo@nxp.com>

--- recipes-bsp/u-boot/u-boot-script-distroboot/ls1028ardb_boot.cmd
+++ recipes-bsp/u-boot/u-boot-script-distroboot/ls1028ardb_boot.cmd
@@ -3,7 +3,7 @@ env exists kernel_image || setenv kernel_image Image;
 env exists devpart_boot || setenv devpart_boot 1;
 env exists devpart_root || setenv devpart_root 2;
 part uuid $devtype $devnum:$devpart_root partuuidr;
-setenv bootargs "console=ttyS0,115200 earlycon=uart8250,mmio,0x21c0500 root=PARTUUID=$partuuidr rw rootwait $othbootargs";
+setenv bootargs "console=ttyS0,115200 earlycon=uart8250,mmio,0x21c0500 root=PARTUUID=$partuuidr rw rootwait video=1920x1080-32@60 cma=640M iommu.passthrough=1 arm-smmu.disable_bypass=0 $othbootargs";
 load $devtype $devnum:$devpart_boot $kernel_addr_r $kernel_image;
 load $devtype $devnum:$devpart_boot $fdt_addr_r $dtb;
 booti $kernel_addr_r - $fdt_addr_r
